152923cf334a598bbdf92da527cdd619d48655e2
Bennett Clapp <bennettclapp@gmail.com>
2021-01-12 22:16:35 -0500

    Updated main.py to include user input.

--- main.py
+++ main.py
@@ -2,14 +2,34 @@ import requests
 import plotly.graph_objects as go
 
 
-movieList = ['1726-iron-man', '1724-the-incredible-hulk', '10138-iron-man-2', '10195-thor', '1771-captain-america-the-first-avenger', '24428-the-avengers','68721-iron-man-3' '76338-thor-the-dark-world', '100402-captain-america-the-winter-soldier', '118340-guardians-of-the-galaxy', '99861-avengers-age-of-ultron', '102899-ant-man', '271110-captain-america-civil-war', '284052-doctor-strange', '283995-guardians-of-the-galaxy-2', '315635-spider-man-homecoming', '284053-thor-ragnarok', '284054-black-panther', '299536-avengers-infinity-war', '363088-ant-man-and-the-wasp', '299537-captain-marvel', '299534-avengers-endgame',]
-movie = "";
+movieList = ['1726-iron-man', '1724-the-incredible-hulk', '10138-iron-man-2', '10195-thor',
+             '1771-captain-america-the-first-avenger', '24428-the-avengers','68721-iron-man-3',
+             '76338-thor-the-dark-world', '100402-captain-america-the-winter-soldier', '118340-guardians-of-the-galaxy',
+             '99861-avengers-age-of-ultron', '102899-ant-man', '271110-captain-america-civil-war',
+             '284052-doctor-strange', '283995-guardians-of-the-galaxy-2', '315635-spider-man-homecoming',
+             '284053-thor-ragnarok', '284054-black-panther', '299536-avengers-infinity-war',
+             '363088-ant-man-and-the-wasp', '299537-captain-marvel', '299534-avengers-endgame']
+movie = ""
 ratingsList = []
 apikey = "85dc8339429fa743fb99f73db9e29777"
 graphList = []
-max = 0;
-maxIndex = 0;
+max = 0
+maxIndex = 0
 
+
+def user():
+    print('Any other movies you would like to include?')
+    input1 = input()
+    if len(input1) > 0:
+        lookupurl = 'https://api.themoviedb.org/3/find/' + input1 + '?api_key=' + apikey + '&language=en-US&external_source=imdb_id'
+        responsemovie = requests.request("GET", lookupurl)
+        movieList.append(input1)
+        user()
+    elif len(input1) == 0:
+        return
+
+
+user()
 for entry in movieList:
     movie = entry
     url = "https://api.themoviedb.org/3/movie/" + movie + "?api_key=" + apikey + "&language=en-US"
@@ -28,6 +48,7 @@ for entry in movieList:
     if currRating > max:
         maxIndex = len(ratingsList)
         max = currRating
+
 
 colors = ['lightslategray',] * len(ratingsList)
 colors[maxIndex - 1] = 'crimson'
